bc9babb6b539dea6c38221babd855d9dc5e882c6
arfanxn <arfan2173@gmail.com>
2025-10-18 01:00:32 +0700

    registered auth related modules

--- internal/infrastructure/di/module.go
+++ internal/infrastructure/di/module.go
@@ -2,11 +2,15 @@ package di
 
 import (
 	"github.com/arfanxn/welding/internal/infrastructure/config"
-	"github.com/arfanxn/welding/internal/infrastructure/database/postgres"
+	"github.com/arfanxn/welding/internal/infrastructure/database"
 	"github.com/arfanxn/welding/internal/infrastructure/http"
 	"github.com/arfanxn/welding/internal/infrastructure/http/jwt"
 	"github.com/arfanxn/welding/internal/infrastructure/logger"
 	"github.com/arfanxn/welding/internal/infrastructure/middleware"
+	permissionDi "github.com/arfanxn/welding/internal/module/permission/infrastructure/di"
+	permissionRoleDi "github.com/arfanxn/welding/internal/module/permission_role/infrastructure/di"
+	roleDi "github.com/arfanxn/welding/internal/module/role/infrastructure/di"
+	roleUserDi "github.com/arfanxn/welding/internal/module/role_user/infrastructure/di"
 	userDi "github.com/arfanxn/welding/internal/module/user/infrastructure/di"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
@@ -19,7 +23,7 @@ var Module = fx.Module("infrastructure",
 	fx.Provide(
 		// Core
 		config.NewConfigFromEnv,
-		postgres.NewPostgresGormDBFromConfig,
+		database.NewPostgresGormDBFromConfig,
 		logger.NewLoggerFromConfig,
 		jwt.NewJWTServiceFromConfig,
 		http.NewRouterFromConfig,
@@ -31,11 +35,14 @@ var Module = fx.Module("infrastructure",
 
 	// Modules
 	userDi.Module,
+	roleDi.Module,
+	roleUserDi.Module,
+	permissionDi.Module,
+	permissionRoleDi.Module,
 
 	// Logger
 	fx.WithLogger(func(logger *logger.Logger) fxevent.Logger {
 		fxLogger := logger.With(zap.String("component", "fx"))
-
 		return &fxevent.ZapLogger{Logger: fxLogger}
 	}),
 
